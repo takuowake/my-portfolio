68d08b660acf870ef42ece669c7bc5a345abe8dc
takuowake <takuowake@gmail.com>
2024-08-06 06:20:15 +0900

    footer.dartを定義

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,44 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../constants/colors.dart';
+
 class Footer extends StatelessWidget {
   const Footer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[900],
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          const Text(
-            '© 2024 Your Name',
-            style: TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.linked_camera, color: Colors.white),
-                onPressed: () {
-                  // Navigate to your social media page
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.message, color: Colors.white),
-                onPressed: () {
-                  // Navigate to your social media page
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.share, color: Colors.white),
-                onPressed: () {
-                  // Navigate to your social media page
-                },
-              ),
-            ],
-          ),
-        ],
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      width: double.maxFinite,
+      alignment: Alignment.center,
+      child: const Text(
+        "Made by Shohruh AK with Flutter 3.10",
+        style: TextStyle(
+          fontWeight: FontWeight.w400,
+          color: CustomColor.whiteSecondary,
+        ),
       ),
     );
   }
