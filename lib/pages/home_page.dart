42e09e19b4ed4715498a1ffccba9690b49c8e21a
takuowake <takuowake@gmail.com>
2024-08-05 21:45:47 +0900

    home_page.dartにkeyを追加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:my_portfolio/widgets/navbar.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
