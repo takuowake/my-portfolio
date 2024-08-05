4389bc054df20acee7693263cbe1464cbed2e7b1
takuowake <takuowake@gmail.com>
2024-08-05 21:49:38 +0900

    footer.dartにkeyを追加

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Footer extends StatelessWidget {
+  const Footer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
