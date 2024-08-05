27e1ac24df42b9a7989b4243df99adba04afd9ca
takuowake <takuowake@gmail.com>
2024-08-06 06:18:50 +0900

    main.dartを定義

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/pages/home_page.dart';
-import 'package:my_portfolio/pages/about_page.dart';
-import 'package:my_portfolio/pages/projects_page.dart';
-import 'package:my_portfolio/pages/contact_page.dart';
+import 'package:my_portfolio/styles/theme.dart';
 
 void main() {
-  runApp(const MyPortfolioApp());
+  runApp(const MyApp());
 }
 
-class MyPortfolioApp extends StatelessWidget {
-  const MyPortfolioApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Portfolio',
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        textTheme: TextTheme(
-          displayLarge: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Colors.black),
-          bodyLarge: TextStyle(fontSize: 18, color: Colors.grey[800]),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: kCustomTheme,
+      title: 'Shohruh AK',
       home: const HomePage(),
-      routes: {
-        '/about': (context) => const AboutPage(),
-        '/projects': (context) => const ProjectsPage(),
-        '/contact': (context) => ContactPage(),
-      },
     );
   }
 }
